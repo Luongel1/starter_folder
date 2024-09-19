05fb217cfd6d283f81e42e5ab127755de31dd07d
Luongel1 <113808559+Luongel1@users.noreply.github.com>
2024-09-19 18:08:28 -0400

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,22 +1,31 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Elizabeth Luong
+# Date: 19 September 2024
+# Contact: elizabethh.luong@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/raw_data/raw_data.csv")
+
 
 cleaned_data <-
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
+  janitor::clean_names() |> 
+  separate(col = time_period,
+           into = c("year", "month"),
+           sep = "-") |> 
+  mutate(date = lubridate::ymd(paste(year, month, "01", sep = "-"))
+  )
+
+#### Save data ####
+write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
+th_mm, wing_length_mm, flying_time_sec_first_timer) |>
   filter(wing_width_mm != "caw") |>
   mutate(
     flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
