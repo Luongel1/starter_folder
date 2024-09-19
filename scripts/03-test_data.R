97e339a1d995eea90858b27f93396fef45edb90e
Luongel1 <113808559+Luongel1@users.noreply.github.com>
2024-09-19 18:08:51 -0400

    Update 03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,22 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Elizabeth Luong
+# Date: 19 September 2024
+# Contact: elizabethh.luong@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
+
+# Test for negative numbers
+data$number_of_marriage |> min() <= 0
+
+# Test for NAs
+all(is.na(data$number_of_marriage))
